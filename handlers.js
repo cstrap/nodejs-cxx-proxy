631d2c266b0b8b319dcab532c1e2db4735510b76
Strap <lab@strap.it>
2012-05-14 13:45:54 +0200

    Code update

--- handlers.js
+++ handlers.js
@@ -23,20 +23,10 @@ exports.index = function(request, response) {
     response.end();
 }
 
-// Proxy API
-function get_body(getUrl, callback) {
-    request_module(getUrl.href, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        util.log("Get uri success");
-        callback(body, response.statusCode, false);
-      } else {
-        util.log("Get uri fail");
-        var statusCode = response != undefined ? response.statusCode : "503";
-        callback("", response, error);
-      }
-    });
-}
 // Handler
+
+var spawn = require("child_process").spawn;
+
 exports.cxx = function(request, response) {
     util.log("Request for handler 'cxx'");
     var cxx = {
@@ -49,26 +39,45 @@ exports.cxx = function(request, response) {
         }
     };
     var queryObj = url.parse(request.url, true).query;
-    util.log(queryObj);
     var parsedUrl = "";
     if (queryObj.uri != undefined) {
         parsedUrl = url.parse(queryObj.uri.replace(/\(|\)/g, ''), true);
-        util.log("parsed url: " + parsedUrl);
-        var cb = function(body, statusCode, error) {
+        
+        util.log("URI: " + parsedUrl.href);
+
+        var response_callback = function(body, statusCode, error) {
             cxx.responseText = body;
             cxx.statusCode = statusCode;
             cxx.error = error;
+            util.log("-----------------------------------------------");
+            util.log("JSON Object");
+            util.log(" - statusCode: " + cxx.statusCode);
+            util.log(" - error     : " + cxx.error);
+            util.log(" - toString  : " + cxx.toString());
+            util.log("-----------------------------------------------");
             response.writeHead(200, {"Content-Type": "plain/text"});
-            var responseString = util.format('"var CXX = "', cxx.toString()); 
+            var responseString = util.format('var CXX = %s', cxx.toString()); 
             if (queryObj.callback != undefined) {
                 responseString = util.format('%s(%s);', queryObj.callback, cxx.toString());
             }
+            util.log(responseString);
             response.write(responseString);
             response.end();
         };
-        setTimeout(cb, 5000);
-        get_body(parsedUrl, cb);
+
+        request_module({uri: parsedUrl.href, timeout: 2000}, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            util.log("Get uri success");
+            response_callback(body, response.statusCode, false);
+          } else {
+            util.log("Get uri fail");
+            var statusCode = response != undefined ? response.statusCode : "503";
+            response_callback("", statusCode, error);
+          }
+        });
+        
     } else {
+        util.log("URI: NULL");
         response.writeHead(200, {"Content-Type": "plain/text"});
         response.write("// Error: no parameters given. //");
         response.end();
